755992ea31563e71f90c6c9c0f13b7f50661d5b5
bagus-k <baagus.kurniawan@gmail.com>
2022-10-31 16:00:00 +0700

    preparation

---
+++ lib/config/api.dart
@@ -0,0 +1,8 @@
+import 'dart:html';
+
+class Api {
+  static const baseUrl = 'http://192.168.100.7/api_money_record';
+
+  static const history = '$baseUrl/history/';
+  static const user = '$baseUrl/user/';
+}
---
+++ lib/config/app_asset.dart
@@ -0,0 +1,4 @@
+class AppAsset {
+  static const logo = 'assets/logo.png';
+  static const user = 'assets/profile.png';
+}
